9e00c2c88f917bcd44746308363a8a54a958e728
felipetojal <felipetojal07@gmail.com>
2026-05-22 18:32:06 -0300

    Unit tests written.

--- contracts/BinhaTokenTest.t.sol
+++ contracts/BinhaTokenTest.t.sol
@@ -10,23 +10,24 @@ import "../contracts/BinhaToken.sol";
  * @notice This contract runs tests on BinhaToken.sol
  */
 contract BinhaTokenTest is Test {
-    BinhaToken token;
-
+    BinhaToken internal token;
+    uint256 internal bin;
     // Hardhat automatically gives a fake address
     address owner = address(this);
 
     // Function setUp runs automatically before the tests
-    function setup() public {
+    function setUp() public {
         token = new BinhaToken();
+        bin = uint256(10 ** token.decimals());
     }
 
     // Auxiliary function to compare strings
-    function compareString(string memory string1, string memory string2) internal returns (bool) {
-        return bytes32(abi.encode(string1)) == bytes32(abi.encode(string2));
+    function compareString(string memory string1, string memory string2) internal pure returns (bool) {
+        return keccak256(bytes(string1)) == keccak256(bytes(string2));
     }
 
     // Testing the name() function 
-    function testName() public {
+    function testName() public view {
         require(
             compareString(token.name(), "BINHA"),
             "TOKEN NAME IS INCORRECT"
@@ -34,15 +35,15 @@ contract BinhaTokenTest is Test {
     }
 
     // Testing the symbol() function
-    function testSymbol() public {
+    function testSymbol() public view {
         require(
-            compareString(token.name(), "BINHA"),
+            compareString(token.symbol(), "BIN"),
             "TOKEN NAME IS INCORRECT"
         );
     }
 
     // Testing the decimals() function
-    function testDecimals() public {
+    function testDecimals() public view {
         assertEq(token.decimals(), 18);
     }
 
@@ -62,24 +63,60 @@ contract BinhaTokenTest is Test {
         address playmobil = address (0x789);
 
         // Transfering tokens from owner to playmobil.
-        bool success = token.transfer(playmobil, 4_000_000);
+        bool success = token.transfer(playmobil, 4_000_000 * bin);
         require (
             success,
             "FAILED TO TRANSFER TOKEN TO PLAYMOBIL"
         );
-        assertEq(token.balanceOf(playmobil), 4_000_000 * uint256(10 ** token.decimals()));
-        uint256 owner_balance = 1_000_000_000 - 4_000_000;
-        assertEq(token.balanceOf(owner), owner_balance * uint256(10 ** token.decimals()));
+        assertEq(token.balanceOf(playmobil), 4_000_000 * bin);
+        uint256 owner_balance = (1_000_000_000 - 4_000_000) * bin;
+        assertEq(token.balanceOf(owner), owner_balance);
 
         // Transfering tokens from owner to nick.
-        success = token.transfer(nick, 1_000_000);
+        success = token.transfer(nick, 1_000_000 * bin);
         require (
             success,
             "FAILED TO TRANSFER TOKEN TO NICK"
         );
-        assertEq(token.balanceOf(nick), 1_000_000 * uint256(10 ** token.decimals()));
-        owner_balance -= 1_000_000;
-        assertEq(token.balanceOf(owner), owner_balance * uint256(10 ** token.decimals()));
+        assertEq(token.balanceOf(nick), 1_000_000 * bin);
+        owner_balance -= (1_000_000 * bin);
+        assertEq(token.balanceOf(owner), owner_balance);
     }
 
+    // Function to test allownce() and approve()
+    function testAllowanceAndApprove() public {
+        address target = address(0x111);
+        uint256 amount = 2_000 * bin;
+        
+        bool success = token.approve(target, amount);
+        require(
+            success,
+            "FAILED TO APPROVE ALLOWANCE"
+        );
+        assertEq(token.allowance(owner, target), amount);
+    }
+
+    // Function to test transferFrom()
+    function testTransferFrom() public {
+        address someone = address(0x109);
+        uint256 allowed = 3_000 * bin;
+        uint256 spend = 1_500 * bin;
+        address chillGuy = address(0x011);
+
+        bool success = token.approve(someone, allowed);
+        require(
+            success,
+            "FAILED TO APPROVE ALLOWANCE"
+        );
+        assertEq(token.allowance(owner, someone), allowed);
+
+        vm.prank(someone);
+        success = token.transferFrom(owner, chillGuy, spend);
+        require(
+            success,
+            "FAILED TO TRANSFER FROM"
+        );
+        assertEq(token.balanceOf(chillGuy), spend);
+        assertEq(token.balanceOf(owner), token.totalSupply() - spend);
+    }
 }
